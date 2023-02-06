cc8bc0673c58a11df9faccdb33981838ab8a5cda
Patrick Sloan <patrickbarrettsloan@outlook.com>
2023-02-06 13:16:25 +0000

    Save

---
+++ SQLNotes.sql
@@ -0,0 +1,35 @@
+-- A place for collating any notes I may have on SQL, as part of Advanced Database Systems
+
+/* 
+SQL Stands for structured qurey languauge. And is used to manage qurey and manipulate databases.
+SQL Allows you to:
+
+- Execute Querys agenst a database.
+- Retreve data from a database.
+- Insert records into a database.
+- Update records in a database.
+- Delete records from a database.
+- Create new databases.
+- Create new tables in a database.
+- Create views in a database.
+- Set permisions.
+??- Create stored procedures.???
+
+While SQL is a standard there are different versions of the laungage. But all should support the
+main commands.
+
+A database is a collection of one or more tables. Each table in a databse is identifyable by a name.
+Which contain rows (records) of data.
+*/
+
+-- SELECT is the most basic SQL command, the following selects everything the 'Customer' table.
+SELECT * FROM Customer;
+
+/*
+Some of the most important SQL comands are listed bellow:
+- 
+- 
+- 
+- 
+- 
+- 
